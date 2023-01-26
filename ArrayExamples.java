4eaa9cb73d999f716db2b7ad163ff2f1e609ff8d
rdshelopal <rdshelopal@gmail.com>
2023-01-26 11:52:32 -0800

    lab3

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -35,6 +35,5 @@ public class ArrayExamples {
     return sum / (arr.length - 1);
   }
 
-
 }
 
--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,25 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+   // Changes the input array to be in reversed order
+   @Test
+   public void reverseInPlace2() {
+
+    int[] arr = {1, 2, 3, 4, 5};
+
+    ArrayExamples.reverseInPlace(arr);
+    
+    assertArrayEquals(new int[] {5,4,3,2,1},arr ); 
+  }
+
+  @Test
+  public void testAverageWithoutLowest()
+  {
+    double[] arr = {2,2,2,2};
+
+    assertEquals(2,ArrayExamples.averageWithoutLowest(arr), 0.0);
+  }
+
+  
 }
