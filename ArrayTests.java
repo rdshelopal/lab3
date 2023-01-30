5f74fd55ba1c35c4913f0cff5671273efce8a9c1
rdshelopal <rdshelopal@gmail.com>
2023-01-29 22:46:59 -0800

    tests

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,28 +4,25 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = {3};
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{3 }, input1);
 	}
+
+  @Test
+  public void testReverseInPlaceFail(){
+  int[] input1 = {1,2,3};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{3,2,1 }, input1);
+  }
 
 
   @Test
-  public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  public void testReversedCase() {
+    int[] input1 = {1,2,3};
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input1));
   }
 
-   // Changes the input array to be in reversed order
-   @Test
-   public void reverseInPlace2() {
-
-    int[] arr = {1, 2, 3, 4, 5};
-
-    ArrayExamples.reverseInPlace(arr);
-    
-    assertArrayEquals(new int[] {5,4,3,2,1},arr ); 
-  }
 
   @Test
   public void testAverageWithoutLowest()
--- ListExamples.java
+++ ListExamples.java
@@ -44,6 +44,5 @@ class ListExamples {
     }
     return result;
   }
-
-
+  
 }
