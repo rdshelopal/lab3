55733d92845a31774192a341b75636a742df4c47
rdshelopal <rdshelopal@gmail.com>
2023-02-07 23:03:55 -0800

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -11,16 +11,16 @@ public class ArrayTests {
 
   @Test
   public void testReverseInPlaceFail(){
-  int[] input1 = {1,2,3};
+  int[] input1 = {};
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{3,2,1 }, input1);
+    assertArrayEquals(new int[]{}, input1);
   }
 
 
   @Test
   public void testReversedCase() {
-    int[] input1 = {1,2,3};
-    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input1));
+    int[] input1 = {};
+    assertArrayEquals(new int[]{}, ArrayExamples.reversed(input1));
   }
 
 
